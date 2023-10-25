fee72c6e8893280888a693c40063715a69e03ff5
Davidm2606 <davidteran2606@hotmail.com>
2023-10-25 01:00:28 -0500

    Commit inicial

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,52 @@
+﻿using APIProductos.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace APIProductos.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        { 
+
+
+        
+        }
+
+
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>().HasData(
+
+                new Producto
+                {
+                    IdProducto = 1,
+                    Nombre = "Producto 1",
+                    Valor = 6,
+                    Cantidad = 10
+                },
+
+                new Producto
+                {
+                    IdProducto = 2,
+                    Nombre = "Producto 2",
+                    Valor = 6,
+                    Cantidad = 20
+                },
+
+                new Producto
+                {
+                    IdProducto = 3,
+                    Nombre = "Producto 3",
+                    Valor = 6,
+                    Cantidad = 30
+                }
+
+                );
+        }
+
+    }
+}
---
+++ Data/Migrations/20231025045444_produto2.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIProductos.Migrations
+{
+    /// <inheritdoc />
+    public partial class produto2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "IdProducto",
+                keyValue: 1,
+                columns: new[] { "Cantidad", "Valor" },
+                values: new object[] { 10, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "IdProducto",
+                keyValue: 2,
+                columns: new[] { "Cantidad", "Valor" },
+                values: new object[] { 20, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "IdProducto",
+                keyValue: 3,
+                columns: new[] { "Cantidad", "Valor" },
+                values: new object[] { 30, 6 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "IdProducto",
+                keyValue: 1,
+                columns: new[] { "Cantidad", "Valor" },
+                values: new object[] { 34, 4});
+
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "IdProducto",
+                keyValue: 2,
+                columns: new[] { "Cantidad", "Valor" },
+                values: new object[] { 34, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "IdProducto",
+                keyValue: 3,
+                columns: new[] { "Cantidad", "Valor" },
+                values: new object[] { 3, 2 });
+        }
+    }
+}
